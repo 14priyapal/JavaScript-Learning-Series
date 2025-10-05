aeb108a3c852f6fb5c187ad7c4cef8adbba4e020
Priya Pal <88577121+14priyapal@users.noreply.github.com>
2025-10-05 17:34:46 +0000

    01 Basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 123
+let accountEmail = "priya@google.com"
+var accountPassword ="**@423"
+accountCity = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
